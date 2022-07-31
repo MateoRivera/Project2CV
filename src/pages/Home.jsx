e5202e83c9c7a341d5764d25e47e6580423f4250
Mateo Rivera <mateo.rivera@udea.edu.co>
2022-07-30 23:35:42 -0500

    Idiomas
    Componente finalizado, hoja de vida finalizada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,11 @@ import personalXP from 'components/static/personalXP';
 import TitleFloating from 'components/text/TitleFloating';
 import data from 'components/static/personalData.json';
 import HobbieCardContainer from 'components/containers/HobbieCard/HobbieCardContainer';
+import LanguageCardContainer from 'components/containers/LanguajeCard/LanguageCardContainer';
 
 const Home = () => {
   const hobbies = [];
+  const languages = [];
   Object.keys(data.hobbies).forEach((key) =>
     // eslint-disable-next-line no-console
     hobbies.push(
@@ -19,6 +21,17 @@ const Home = () => {
       />
     )
   );
+
+  Object.keys(data.languages).forEach((key) =>
+    // eslint-disable-next-line no-console
+    languages.push(
+      <LanguageCardContainer
+        title={data.languages[key].language}
+        body={data.languages[key].level}
+      />
+    )
+  );
+
   return (
     <main className='h-full p-3 flex flex-col gap-3 md:items-center'>
       <div className='md:max-w-3xl'>
@@ -49,8 +62,8 @@ const Home = () => {
       <div className='flex flex-col gap-1 w-full md:max-w-3xl lg:max-w-full'>
         <TitleFloating title='Idiomas' />
         <div className='overflow-x-auto'>
-          <div className='flex gap-2 w-fit lg:w-full lg:justify-between'>
-            {hobbies}
+          <div className='flex gap-2 w-fit md:w-full md:justify-between'>
+            {languages}
           </div>
         </div>
       </div>
